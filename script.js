2a24d4e570c55614bd26d57dba405e11721129cb
Esther <queenestheradekanm@gmail.com>
2024-12-22 19:41:13 +0100

    We finally, added the js part and completed creating our calculator.

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const display = document.getElementById("display");
-const buttons =document.querySelectorAll('.btn, .btn-operator, .btn-eual');
-buttons.forEACH(button => {
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn, .btn-operator, .btn-equal');
+let input ="";
+buttons.forEach(button =>{
     button.addEventListener('click',() =>{
-        const value = button.innerText;
-        if(value === 'C'){
-            display.innerText=""
-        }else if (value === '=') {
-            try {
-                display.innerText = eval(display.inner)
-            }
-        }       
-    })
-})
+        const value = button.textContent;
+        if(value ==='CLR'){
+            input ="";
+            display.textContent="";
+        }
+        else if(value ==="="){
+            input =input.replace(/×/g, '*').replace(/÷/g,"/");
+            display.textContent=eval(input);
+            input=display.textContent;
+        }
+        else{
+            input+=value;
+            display.textContent=input;
+        }
+    });
+});
